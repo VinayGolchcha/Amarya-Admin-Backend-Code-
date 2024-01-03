d251fa8f4e4e231308631051664482cfba1d711a
Sanjana_Jain022 <sanjana.jain@shephertz.com>
2024-01-03 13:22:50 +0530

    #user registration api complete

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ await setupDatabase();
 import express, { json } from 'express';
 import { config } from 'dotenv';
 import cors from 'cors';
+import userRoutes from './v1/user/routes/userRoutes.js'
 
 const app = express();
 config();
 app.use(json());
 app.use(cors());
-// Import & Define API versions
 
+// Import & Define API versions
+app.use('/api/v1', userRoutes);
 app.use('/', (req, res) => {
   res.send("Hey, I'm online now!!")
 });
