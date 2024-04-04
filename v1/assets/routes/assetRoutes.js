70f4793af6853914787b8f6de653004594e0fa1f
shubham soni <ssoni190@gmail.com>
2024-04-04 16:56:28 +0530

    Revert "#change in route"
    This reverts commit 998fb1d8c1b0da09222f2a96dbba51c3312cf5b6.

--- v1/assets/routes/assetRoutes.js
+++ v1/assets/routes/assetRoutes.js
@@ -10,7 +10,7 @@ import {createAsset, assetRequest, fetchUserAssets, deleteAsset, fetchAssets, up
 app.post('/admin/create-asset',crAssVal, createAsset);
 app.post('/asset-request',assReqVal, assetRequest);
 app.post('/user-asset',fetUserAssVal, fetchUserAssets);
-app.get('/admin/fetch-assigned-assets', fetchAssets);
+app.get('/admin/fetch-assets', fetchAssets);
 app.delete('/admin/delete-asset',delAssVal, deleteAsset);
 app.put('/admin/update-asset/:id',upAssVal, updateAsset);
 
