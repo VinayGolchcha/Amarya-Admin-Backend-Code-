46049eddf1b123a4650a8f13d5a6be1cb8593850
tanshul28 <tanshul578@gmail.com>
2024-04-24 14:34:29 +0530

    changes

--- v1/dashboard/controllers/dashboardControllers.js
+++ v1/dashboard/controllers/dashboardControllers.js
@@ -1,16 +1,25 @@
 import dotenv from "dotenv";
 import { validationResult } from "express-validator";
-import { successResponse } from "../../../utils/response.js";
-import { fetchEmployeeCountQuery } from "../query/dashboardQuery.js";
+import { fetchAnnouncementsQuery, fetchActivityQuery, getUserProfileQuery, feedbackFormQuery } from "../models/dashBoardQuery.js";
+import { successResponse, errorResponse, notFoundResponse } from "../../../utils/response.js";
+import dotenv from "dotenv";
+// import { incrementId } from "../../helpers/functions.js"
 dotenv.config();
-export const fetchEmployeeCount = async(req, res, next) => {
+import cloudinaryConfig from "../../../config/cloudinary.js";
+
+
+
+
+export const fetchAnnouncements = async (req, res, next) => {
     try {
-        let [data] = await fetchEmployeeCountQuery();
-        const responseData = data.map(row => ({
-            team: row.team,
-            employeeCount: row.employee_count
-          }));
-        return successResponse(res, responseData, 'Employee Count Fetched Successfully');
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return errorResponse(res, errors.array(), "")
+    }
+
+    let [data] = await fetchAnnouncementsQuery();
+    return successResponse(res, data, 'Announcements Fetched Successfully');
     } catch (error) {
         next(error);
     }
