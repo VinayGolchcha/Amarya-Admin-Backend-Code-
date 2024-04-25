d22fdd7067cc94a3bfa9cf340d2087ea38cd3481
Sanjana_Jain022 <sanjana.jain@shephertz.com>
2024-04-25 17:16:06 +0530

    # performance logic written for employees only and updation in user table

--- v1/helpers/functions.js
+++ v1/helpers/functions.js
@@ -37,3 +37,30 @@ export const createDynamicUpdateQuery = async(table, condition, req_data)=>{
     });
     return {updateQuery, updateValues};
 }
+
+export const getWorkingDaysCountPreviousMonth = () => {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = currentDate.getMonth(); // Current month index (0-based)
+
+    // Get the first day of the previous month
+    const firstDayOfPreviousMonth = new Date(year, month - 1, 1);
+
+    // Get the last day of the previous month
+    const lastDayOfPreviousMonth = new Date(year, month, 0);
+
+    let count = 0;
+
+    // Iterate through the days of the previous month
+    for (let day = firstDayOfPreviousMonth.getDate(); day <= lastDayOfPreviousMonth.getDate(); day++) {
+        const currentDate = new Date(year, month - 1, day);
+        const currentDayOfWeek = currentDate.getDay(); // Day of the week index (0 for Sunday, 1 for Monday, ..., 6 for Saturday)
+        
+        // Check if the current day is a weekday (Monday to Friday)
+        if (currentDayOfWeek >= 1 && currentDayOfWeek <= 5) {
+            count++;
+        }
+    }
+
+    return count;
+}
--- v1/worksheets/routes/worksheetRoutes.js
+++ v1/worksheets/routes/worksheetRoutes.js
@@ -3,6 +3,7 @@ const app = express()
 const router = Router();
 // import authenticateToken from '../../../middlewares/auth.js';
 import {createUserWorksheet, updateUserWorksheet, deleteUserWorksheet, fetchUserWorksheet} from '../controllers/worksheetController.js';
+import {calculatePerFormanceForEachEmployee} from "../controllers/perfromanceController.js"
 import {createWorksheetVal, updateWorksheetVal, deleteUserWorksheetVal, fetchUserWorksheetVal} from '../../../utils/validation.js';
 // router.use(authenticateToken)
 
@@ -12,7 +13,7 @@ app.post('/create-worksheet',createWorksheetVal, createUserWorksheet);
 app.put('/update-worksheet/:id/:emp_id',updateWorksheetVal, updateUserWorksheet);
 app.delete('/delete-worksheet/:id/:emp_id',deleteUserWorksheetVal, deleteUserWorksheet)
 app.get("/fetch-user-worksheet/:emp_id",fetchUserWorksheetVal, fetchUserWorksheet)
-
+app.get("/calculate-performance", calculatePerFormanceForEachEmployee)
 
 app.use("/", router);
 
