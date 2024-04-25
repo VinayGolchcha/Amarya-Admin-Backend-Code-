29dce29b41a4175b5433481c96c5b7ce99bf04d7
Sanjana_Jain022 <sanjana.jain@shephertz.com>
2024-04-25 17:16:06 +0530

    # performance logic written for employees only and updation in user table

--- v1/worksheets/models/performanceQuery.js
+++ v1/worksheets/models/performanceQuery.js
@@ -1,11 +1,46 @@
 import pool from "../../../config/db.js"
 
-export const getCategoryTotalPoints = async () => {
+export const getCategoryTotalPointsQuery = async () => {
     try {
         let query = `SELECT SUM(points) AS total_points FROM categories;`
         return pool.query(query);
     } catch (error) {
         console.error("Error executing getCategoryTotalPoints:", error);
+        throw error;
+    }
+}
+
+export const getAllEmployeesQuery = () => {
+    try {
+        let query = `SELECT emp_id FROM users`
+        return pool.query(query);
+    } catch (error) {
+        console.error("Error executing getAllEmployees:", error);
+        throw error;
+    }
+}
+
+export const getUserPointsQuery = (array) => {
+    try {
+        let query = `SELECT w.emp_id,
+            SUM(c.points) AS total_earned_points
+            FROM worksheets w
+            JOIN categories c ON w.category_id = c._id
+            WHERE MONTH(w.date) = ?
+            GROUP BY w.emp_id;`
+        return pool.query(query, array);
+    } catch (error) {
+        console.error("Error executing getAllEmployees:", error);
+        throw error;
+    }
+}
+
+export const updateUserPerformanceQuery = (array) =>{
+    try {
+        let query = `UPDATE users SET performance = ? WHERE emp_id = ?`
+        return pool.query(query, array);
+    } catch (error) {
+        console.error("Error executing updateUserPerformanceQuery:", error);
         throw error;
     }
 }
