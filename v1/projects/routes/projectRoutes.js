6f2ec274c49fc36e087103011b5ffd08e53c65e3
tanshul28 <tanshul578@gmail.com>
2024-04-29 10:40:51 +0530

    changes

--- v1/projects/routes/projectRoutes.js
+++ v1/projects/routes/projectRoutes.js
@@ -1,7 +1,7 @@
 import express, { Router } from 'express';
-import { createProjectVal, deleteIdVal, updateProjectVal } from '../../../utils/validation.js';
+import { createProjectVal, deleteIdVal, updateProjectVal,createUserProjectVal,userUpdateProjectVal } from '../../../utils/validation.js';
 import { createProject, deleteProject, fetchProjects, updateProject } from '../controllers/projectController.js';
-import {createUserProject,fetchUserProjects} from '../controllers/userProjectController.js';
+import {createUserProject,fetchUserProjects,userUpdateProject} from '../controllers/userProjectController.js';
 const app = express()
 const router = Router();
 
@@ -9,8 +9,6 @@ app.post('/admin/create-project', createProjectVal, createProject);
 app.put('/admin/update-project/:id/:category_id',updateProjectVal, updateProject);
 app.get('/admin/fetch-all-projects', fetchProjects);
 app.delete('/admin/delete-project/:id/:category_id',deleteIdVal, deleteProject);
-app.post('/user-profile-project',createUserProject)
-app.get('/fetch-user-projects/:project_id', fetchUserProjects);
 
 
 app.use("/", router);
