262055ddf2647017e414d4d505c5ec0610832e20
Ujjwal <ujjwal.2000upadhyay@gmail.com>
2024-05-01 20:39:08 +0530

    Comments are removed from the dashboard

--- v1/dashboard/controllers/dashboardControllers.js
+++ v1/dashboard/controllers/dashboardControllers.js
@@ -6,21 +6,8 @@ dotenv.config();
 export const fetchEmployeeCount = async(req, res, next) => {
     try {
         let [data] = await fetchEmployeeCountQuery();
-        const responseData = data.map(row => ({
-            team: row.team,
-            employeeCount: row.employee_count
-          }));
-        return successResponse(res, responseData, 'Employee Count Fetched Successfully');
+        return successResponse(res, data, 'Employee Count Fetched Successfully');
     } catch (error) {
         next(error);
     }
 }
-
-// export const fetchTeamPerformance = async(req, res, next) => {    
-//     try{
-//         let [data] = await fetchTeamPerformanceQuery();
-//         return successResponse(res, data, 'performace based on teams data fetched succeccfully');
-//     }catch(error){
-//         next(error);
-//     }
-// }
