b71d021294a2c3139d0e0ca5180aa5e308300647
shubham soni <ssoni190@gmail.com>
2024-05-03 20:07:05 +0530

    #added cron check and filter announcement API

--- v1/activity/controllers/activityController.js
+++ v1/activity/controllers/activityController.js
@@ -124,7 +124,8 @@ export const filterActivityByDate = async (req, res, next) => {
       return errorResponse(res, errors.array(), "");
     }
     const date = req.body.date;
-    let array = await fetchActivityQuery([date]);
+    let event_type = "activity";
+    let array = await fetchActivityQuery([event_type]);
     const result = array[0].filter((item) => item.created_at.toISOString().includes(date));
     if (result.length === 0) {
       return notFoundResponse(res, result, "Activity not found in specified date");
