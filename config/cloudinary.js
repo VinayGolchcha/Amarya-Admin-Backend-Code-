188271211da2f058bc1ff31bf505a27a7a4ea34b
tanshul28 <tanshul578@gmail.com>
2024-05-09 11:02:05 +0530

    Merge branch 'feature-user-project' of https://github.com/VinayGolchcha/Amarya-Admin-Backend-Code- into feature-user-dashboard
    # Conflicts:
    #	v1/users/controllers/userDashboardController.js
    #	v1/users/models/userDashboardQuery.js
    #	v1/users/routes/userdashboardRoutes.js

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import {v2 as cloudinary} from 'cloudinary';
 
-export let cloudinaryConfig = cloudinary.config({
+const cloudinaryConfig = cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.API_KEY,
     api_secret: process.env.API_SECRET,
