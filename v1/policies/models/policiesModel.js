89e90a3545b9f3b8db14f431bbdf8ad109c7d974
Ujjwal <ujjwal.2000upadhyay@gmail.com>
2024-05-14 15:29:38 +0530

    Changes on the policies

--- v1/policies/models/policiesModel.js
+++ v1/policies/models/policiesModel.js
@@ -2,7 +2,7 @@ const policiesTable = `
 CREATE TABLE IF NOT EXISTS policies (
     _id int NOT NULL AUTO_INCREMENT,
     policy_type varchar(20) NOT NULL,
-    policy_description TEXT,
+    image_data LONGBLOB,
     created_at datetime DEFAULT CURRENT_TIMESTAMP,
     updated_at datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (_id)
