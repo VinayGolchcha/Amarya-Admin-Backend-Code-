b3f20feee9a126c6cf7aa637a682f75db0a6d113
Sanjana_Jain022 <sanjana.jain@shephertz.com>
2024-05-23 18:17:37 +0530

    # user-dashboard images api completed

--- v1/users/routes/userdashboardRoutes.js
+++ v1/users/routes/userdashboardRoutes.js
@@ -2,13 +2,14 @@ import express, { Router } from 'express';
 const app = express()
 const router = Router();
 
-import { userDashboard, feedbackForm, fetchFeedbackData} from '../controllers/userDashboardController.js';
+import { userDashboard, feedbackForm, fetchFeedbackData, getDashboardImages} from '../controllers/userDashboardController.js';
 import { feedbackVal } from '../../../utils/validation.js';
 
 
 app.post('/user-dashboard-feedback', feedbackVal,feedbackForm);
 app.get('/admin/fetch-user-feedback', fetchFeedbackData);
 app.get('/user-dashboard/:emp_id', userDashboard);
+app.get('/get-dashboard-images', getDashboardImages);
 
 app.use("/", router);
 export default app;
