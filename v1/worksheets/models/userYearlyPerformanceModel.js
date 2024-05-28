9e53654392a9045b7bcdfb2ef55dc23f347f2060
Sanjana_Jain022 <sanjana.jain@shephertz.com>
2024-05-28 20:52:17 +0530

    # user dashboard points api for graph completed

--- v1/worksheets/models/userYearlyPerformanceModel.js
+++ v1/worksheets/models/userYearlyPerformanceModel.js
@@ -2,8 +2,8 @@ const userYearlyPerformanceTable = `
 CREATE TABLE IF NOT EXISTS userYearlyPerformance (
     _id int NOT NULL AUTO_INCREMENT,
     emp_id varchar(255) NOT NULL,
-    year int DEFAULT NULL,
-    points int DEFAULT NULL,
+    year int DEFAULT 0,
+    points int DEFAULT 0,
     created_at datetime DEFAULT CURRENT_TIMESTAMP,
     updated_at datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (_id),
