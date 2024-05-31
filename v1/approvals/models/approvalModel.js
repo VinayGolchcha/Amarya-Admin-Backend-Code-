21ce28fdf23fad6b07de36696886b2e7e7659f26
Sanjana_Jain022 <sanjana.jain@shephertz.com>
2024-05-31 18:03:51 +0530

    # approval model revert changes

--- v1/approvals/models/approvalModel.js
+++ v1/approvals/models/approvalModel.js
@@ -3,7 +3,12 @@ CREATE TABLE IF NOT EXISTS approvals (
     _id int NOT NULL AUTO_INCREMENT,
     emp_id varchar(255) NOT NULL,
     foreign_id varchar(255),
-    
+    request_type varchar(255) NOT NULL,
+    item varchar(255) NOT NULL,
+    status ENUM('rejected', 'approved', 'pending') DEFAULT 'pending' NOT NULL,
+    subject varchar(255) NOT NULL,
+    body varchar(255) NOT NULL,
+    request_date date,
     approval_date date,
     issued_from date,
     issued_till date,
