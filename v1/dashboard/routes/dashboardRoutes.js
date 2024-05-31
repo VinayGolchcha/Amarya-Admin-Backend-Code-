891331fccc9ee34f4ae088cb83bb85af8515ceaa
Sanjana_Jain022 <sanjana.jain@shephertz.com>
2024-05-31 17:39:21 +0530

    # admin dashboard approavals api and fetching announcement and activity apis completed.

--- v1/dashboard/routes/dashboardRoutes.js
+++ v1/dashboard/routes/dashboardRoutes.js
@@ -1,9 +1,11 @@
 import express, { Router } from 'express';
-import { adminDashboard } from '../controllers/dashboardController.js';
+import { adminDashboard, fetchApprovalData, fetchActivityAndAnnouncementData } from '../controllers/dashboardController.js';
 const app = express()
 const router = Router();
 
 app.get('/admin/admin-dashboard', adminDashboard);
+app.get('/admin/fetch-approval-data', fetchApprovalData);
+app.get('/admin/fetch-activity-announcement', fetchActivityAndAnnouncementData);
 
 
 app.use("/", router);
