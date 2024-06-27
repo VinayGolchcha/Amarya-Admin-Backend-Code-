e5e1ebaa35bcfdecd724582e7026b34bfaf5ab68
shubham soni <ssoni190@gmail.com>
2024-06-27 12:19:31 +0530

    #policy link in response

--- v1/policies/models/policiesModel.js
+++ v1/policies/models/policiesModel.js
@@ -2,7 +2,7 @@ const policiesTable = `
 CREATE TABLE IF NOT EXISTS policies (
     _id int NOT NULL AUTO_INCREMENT,
     policy_heads varchar(1080) NOT NULL,
-    file_data LONGBLOB,
+    file_data varchar(255),
     created_at datetime DEFAULT CURRENT_TIMESTAMP,
     updated_at datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (_id)
