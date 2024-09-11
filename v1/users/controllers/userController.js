cf094a7c4c39c38b4f590a431fea335f99e131ac
Sanjana_Jain022 <sanjana.jain@shephertz.com>
2024-09-11 10:57:25 +0530

    # onrender.com set for domain

--- v1/users/controllers/userController.js
+++ v1/users/controllers/userController.js
@@ -406,7 +406,7 @@ export const userGhostLogin = async (req, res) => {
         });
         await insertTokenQuery(token, currentUser._id);
         res.cookie('token', token, {
-            domain: 'https://messenger-app-amarya-fe.vercel.app/',
+            domain: '.onrender.com',
             path: '/',
             httpOnly: true,
             sameSite: 'None',
