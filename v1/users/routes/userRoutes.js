26717d7d64e2bbed35ca1be711fe97a6e81ebbf9
shubham soni <ssoni190@gmail.com>
2024-10-23 12:49:24 +0530

    #emp list API

--- v1/users/routes/userRoutes.js
+++ v1/users/routes/userRoutes.js
@@ -4,10 +4,11 @@ const app = express()
 const router = Router();
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
-import {userRegistration, userLogin, userLogout, updateUserPassword, verifyEmailForPasswordUpdate, sendOtpForPasswordUpdate, getUserProfile, updateUserProfile, fetchAllEmployeeIds, userGhostLogin} from '../controllers/userController.js';
+import {userRegistration, userLogin, userLogout, updateUserPassword, verifyEmailForPasswordUpdate, sendOtpForPasswordUpdate, getUserProfile, updateUserProfile, fetchAllEmployeeIds, userGhostLogin, fetchAllEmployeeList} from '../controllers/userController.js';
 import {userRegVal, userLogVal, logOutVal, upPassVal, sendOtpVal, verifyOtpVal, getUserVal, loginVal} from '../../../utils/validation.js';
 import {authenticateUserSession} from "../../../middlewares/userAuth.js"
 import {authenticateUserAdminSession} from "../../../middlewares/userAdminAuth.js"
+import { authenticateAdminSession } from '../../../middlewares/adminAuth.js';
 
 
 app.post('/admin/register', upload.single('file'), userRegVal, userRegistration);
@@ -20,7 +21,7 @@ app.post('/get-user-profile/:emp_id', authenticateUserAdminSession, getUserVal, 
 app.put('/update-user-profile/:id' , authenticateUserAdminSession, upload.single('file'), updateUserProfile);
 app.get('/fetch-all-employee-ids',authenticateUserAdminSession, fetchAllEmployeeIds);
 app.post('/ghost-login', loginVal, userGhostLogin);
-
+app.get('/admin/fetch-all-employee-list',authenticateAdminSession, fetchAllEmployeeList)
 
 app.use("/", router);
 
