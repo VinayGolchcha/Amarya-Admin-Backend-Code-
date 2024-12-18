3f5efd9c4815c4ff3affd546df8f1cafee6813b3
b0ssh3r3 <kushwahadeepanshujbp@gmail.com>
2024-12-18 15:44:55 +0530

    image fix

--- utils/socket.js
+++ utils/socket.js
@@ -6,16 +6,24 @@ export const socketEvents = (io) => {
       console.log('Client connected');
   
       socket.on('detections', async (data) => {
-        console.log('Received detections:', data.detections[0].class_name);
-        console.log('Received detections:', data.detections[0].confidence);
-        console.log('Received detections:', data.detections[0].bounding_box);
-        console.log('Received time:', data.detections[0].detection_time);
+        console.log('Received class_name:', data.detections[0].class_name);
+        console.log('Received confidence:', data.detections[0].confidence);
+        console.log('Received bounding_box:', data.detections[0].bounding_box);
+        console.log('Received detection_time:', data.detections[0].detection_time);
         console.log('Received URL:', data.rtsp_url);
         console.log('stream_id:', data.stream_id);
+        // console.log('Received frame_data:', data.image);
         if (data.detections[0]?.confidence > 0.50) {
           const uniqueData = Object.values(
             data.detections.reduce((acc, item) => {
-              acc[item.class_name] = item;
+              // acc[item.class_name] = item;
+
+              acc[item.class_name] = { 
+                ...item, 
+                image: data.image
+              };
+
+
               return acc;
             }, {})
           );
